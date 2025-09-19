57816115bdf5b5ef6c0cc20b185976d6bd6ae087
Nadson Sousa <nadsonbcs95@gmail.com>
2025-09-19 12:42:10 -0300

    fix: styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function HomePage() {
           <div className="relative z- mx-auto mt-10">
             <h1 className="font-poppins text-5xl mt-12"><span className="font-bold">Soluções logísticas</span> com a sua identidade!</h1>
             <p className="font-quicksand text-lg mt-5">Empodere PMEs e e-commerces com uma plataforma whitelabel flexível para dominar sua logística.</p>
-            <a href="#solucoes" className="bg-[#ef7d00] text-white text-center font-bold py-4 px-8 text-lg rounded-md my-10 flex flex-row items-center gap-2 justify-center transform hover:scale-105 w-50 mx-auto hover:bg-[#914b00] active:bg-[#914b00] focus:bg-[#914b00] cursor-pointer transition-all duration-300">
+            <a href="#solucoes" className="bg-[#ef7d00] text-white text-center font-bold py-4 px-8 text-lg shadow-lg rounded-md my-10 flex flex-row items-center gap-2 justify-center transform hover:scale-105 w-50 mx-auto hover:bg-[#914b00] active:bg-[#914b00] focus:bg-[#914b00] cursor-pointer transition-all duration-300">
               Saiba mais <HiCursorClick size={20} className="text-white" />
             </a>
           </div>
@@ -104,7 +104,7 @@ export default function HomePage() {
         <Image src={logobgWhite} className="mb-10" alt="Dropplace Logo" width={240} height={240} />
         <h2 className="font-poppins text-4xl font-bold">Pronto para transformar sua logística?</h2>
         <p className="font-quicksand text-lg mt-4">Entre em contato e descubra como a Dropplace pode impulsionar o crescimento da sua empresa.</p>
-        <a href="https://w.app/dropplace-startup" className="bg-[#ef7d00] text-white font-poppins font-bold py-4 px-8 rounded-md inline-block mt-8 transition-all transform hover:scale-105 active:scale-105 hover:bg-[#914b00] active:bg-[#914b00] focus:bg-[#914b00]">Fale com um especialista</a>
+        <a href="https://w.app/dropplace-startup" className="bg-[#ef7d00] shadow-lg text-white font-poppins font-bold py-4 px-8 rounded-md inline-block mt-8 transition-all transform hover:scale-105 active:scale-105 hover:bg-[#914b00] active:bg-[#914b00] focus:bg-[#914b00]">Fale com um especialista</a>
       </section>
 
       <footer className="bg-black text-white py-10 px-5 text-center">
